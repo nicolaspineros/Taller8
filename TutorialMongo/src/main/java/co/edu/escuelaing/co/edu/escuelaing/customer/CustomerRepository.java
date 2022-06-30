5fd2129ae16c6116edd7753213b026e5e54a0079
unknown <oscar.pineros-c@labinfo.is.escuelaing.edu.co>
2022-06-30 11:47:06 -0500

    Taller

---
+++ TutorialMongo/src/main/java/co/edu/escuelaing/co/edu/escuelaing/customer/CustomerRepository.java
@@ -0,0 +1,12 @@
+package co.edu.escuelaing.co.edu.escuelaing.customer;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+    public Customer findByFirstName(String firstName);
+    public List<Customer> findByLastName(String lastName);
+
+}
